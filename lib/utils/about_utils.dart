2eda3fc1f9877bde244f09100425e6672b61284b
salah <salah@iocod.com>
2022-08-16 08:18:57 +0530

    initial commit

---
+++ lib/utils/about_utils.dart
@@ -0,0 +1,7 @@
+class AboutUtils {
+  static const String aboutMeHeadline =
+      'I\'m Muhammad Salah, a Flutter developer and UI designer.';
+
+  static const String aboutMeDetail =
+      'I am a Flutter developer with 9+ months of experience who is comfortable of working with dart, provider State management, fire base ,bit-bucket, git ,jira. Extensive knowledge of Flutter Framework and creative in making android and ios apps. An ability to work together with a large team. I have been developing mobile apps for over 9 months now. I have completed my internship from Iocod Infotech and from brototype ,and worked with a large team for devolping mobile apps both in android and IOS.';
+}
