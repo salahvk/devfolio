7e2234a03bf499491475bf401577abe2d111e4b0
mo <salahkoyilandy@gmail.com>
2022-10-13 12:25:29 +0530

    Personal project field images added

---
+++ lib/components/assets_manager.dart
@@ -0,0 +1,7 @@
+class ImageAssets {
+  static const String football = 'assets/salah/football.jpg';
+  static const String delivery = 'assets/salah/delivery.jpg';
+  static const String order = 'assets/salah/order.png';
+  static const String cognizium = 'assets/salah/cognizium.jpg';
+  // static const String football = 'assets/salah/football.jpg';
+}
--- lib/utils/project_utils.dart
+++ lib/utils/project_utils.dart
@@ -1,18 +1,20 @@
+import 'package:folio/components/assets_manager.dart';
+
 class ProjectUtils {
   static const List<String> banners = [
     "assets/salah/salaflix (1).png",
-    "assets/projects/quranB.png",
-    "assets/projects/medkitB.png",
-    "assets/projects/hereiamB.png",
-    "assets/projects/covidB.png",
+    ImageAssets.football,
+    ImageAssets.delivery,
+    ImageAssets.order,
+    ImageAssets.cognizium,
   ];
 
   static const List<String> icons = [
     "assets/salah/salaflix_removebg.png",
-    "assets/projects/quran.png",
-    "assets/projects/medkit.png",
-    "assets/projects/hereiam.png",
-    "assets/projects/covid.png",
+    ImageAssets.football,
+    ImageAssets.delivery,
+    ImageAssets.order,
+    ImageAssets.cognizium,
     "assets/projects/messenger.png",
     "assets/projects/flutter.png",
     "assets/projects/earbender.png",
@@ -23,10 +25,10 @@ class ProjectUtils {
 
   static const List<String> titles = [
     "Salaflix TMDB App",
-    "The Holy Qur'an",
-    "MedKit",
-    "Here I Am",
-    "COVID-19",
+    "Football Scores App",
+    "Delivery App",
+    "Product Order app",
+    "Cognizium campus fest",
     "Messenger Chat Head UI",
     "flutter.dev - Flutter Web",
     "Earbender",
@@ -37,10 +39,10 @@ class ProjectUtils {
 
   static const List<String> description = [
     "It's the small TMDB app where you can know everything about movies and Tv series. You can also watch trailers of these contents.",
-    "Application of Holy book of Muslims, Al-Qur'an. Developed using Flutter. Powered with live RestAPI given in README.md",
-    "A Phramacy app developed using Flutter powered with Firebase as database with Doctor and Patient panels.",
-    "Here I am is an Alert app that Sends alert SMS holding your location (Address and Google Maps) to your loved ones.",
-    "A live trakcer for COVID19 stats across the Globe and my Home country Pakistan. It uses APIs so the data is live.",
+    "Its a small app to know live football scores of major tournaments. Football Api's is used in it .More features will be added",
+    "An app which notifies whether the product is delivered or not .Backend developed using firebase",
+    "Shops can register in order admin app and can list there products .Customers from the same locality can view all these shops and there products",
+    "A website developed with flutter to register contestents and there programs in campus fest",
     "Tried to clone the famous Chat Heads by Facebook Messenger using Flutter. Its basically in-app not exactly like the original chat head.",
     "Flutter Official Site clone using Flutter Web. Try the live site here. For the time being its not responsive fully so there might be responsiveness issues.",
     "A concept of Music App developed using Flutter. Having functionality of playing Audio.",
@@ -51,10 +53,10 @@ class ProjectUtils {
 
   static const List<String> links = [
     "https://github.com/salahvk/salaflix",
-    "https://github.com/mhmzdev/The_Holy_Quran_App",
-    "https://github.com/mhmzdev/MedKit-Pharmacy-App-Using-Flutter",
-    "https://github.com/mhmzdev/Here-I-Am-Alert-App",
-    "https://github.com/mhmzdev/Covid19-Tracker-App",
+    "https://github.com/salahvk/football_scores",
+    "https://github.com/salahvk/Delivery",
+    "https://github.com/salahvk/order-app",
+    "https://github.com/salahvk/cognizium",
     "https://github.com/mhmzdev/Messenger-Chat-Head-Flutter-UI",
     "https://github.com/mhmzdev/flutter.dev-Flutter-Web-Clone",
     "https://github.com/mhmzdev/Earbender_Music_App",
